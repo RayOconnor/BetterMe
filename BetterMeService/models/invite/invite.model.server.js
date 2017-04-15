782dd2b8875d06aafd0f505a97255e5b9f68cfdd
RayOconnor <roconnorc@gmail.com>
2017-04-15 00:28:33 -0400

    add basic invite functionality

---
+++ BetterMeService/models/invite/invite.model.server.js
@@ -0,0 +1,66 @@
+module.exports = function () {
+
+  var api = {
+    createInvite: createInvite,
+    findInviteById: findInviteById,
+    updateInvite: updateInvite,
+    deleteInvite: deleteInvite
+  };
+
+  var q = require('q');
+  var mongoose = require('mongoose');
+
+  var InviteSchema = require('./invite.schema.server')();
+  var InviteModel = mongoose.model('InviteModel', InviteSchema);
+
+  return api;
+
+  function createInvite(invite) {
+    return InviteModel.create(invite);
+  }
+
+  function findInviteById(inviteId) {
+    var d = q.defer();
+
+    InviteModel
+      .findById(inviteId, function (err, invite) {
+        if(err) {
+          d.reject(err);
+        } else {
+          d.resolve(invite);
+        }
+      });
+
+    return d.promise;
+  }
+
+  function updateInvite(inviteId, invite) {
+    var d = q.defer();
+
+    InviteModel
+      .findOneAndUpdate({'_id': inviteId}, invite, function (err, invite) {
+        if(err) {
+          d.reject(err);
+        } else {
+          d.resolve(invite);
+        }
+      });
+
+    return d.promise;
+  }
+
+  function deleteInvite(inviteId) {
+    var d = q.defer();
+
+    InviteModel
+      .remove({'_id': inviteId}, function (err, status) {
+        if(err) {
+          d.reject(err);
+        } else {
+          d.resolve(status);
+        }
+      });
+
+    return d.promise;
+  }
+};
