07a2dd8ad44613e66358cdca4bda165be20fb21f
RayOconnor <roconnorc@gmail.com>
2017-04-15 15:04:44 -0400

    correct regimen dalete

--- BetterMeService/services/regimen.service.server.js
+++ BetterMeService/services/regimen.service.server.js
@@ -3,6 +3,7 @@ module.exports = function (app, RegimenModel, UserModel) {
   //app.get("/api/regimen", getRegimens);
   app.get("/api/regimen/user/:userId", getRegimensForUser);
   app.put("/api/regimen/:regimenId", updateRegimen);
+  app.delete("/api/regimen/:regimenId", deleteRegimen);
 
   function createRegimen(req, res) {
     var newRegimen = req.body;
@@ -36,6 +37,17 @@ module.exports = function (app, RegimenModel, UserModel) {
       .updateRegimen(regimenId, updatedRegimen)
       .then(function(regimen) {
         res.json(regimen.toObject());
+      }, function (error) {
+        res.sendStatus(500).send(error);
+      });
+  }
+
+  function deleteRegimen(req, res) {
+    var regimenId = req.params.regimenId;
+    RegimenModel
+      .deleteRegimen(regimenId)
+      .then(function(regimen) {
+        res.json(regimen);
       }, function (error) {
         res.sendStatus(500).send(error);
       });
